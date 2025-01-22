5af8c5b3621f1453020f4d8d3a1f6de0697335a3
Danyl Brahin <inicerer141@gmail.com>
2025-01-22 23:31:22 +0200

    Refactor opening of the player

--- app/src/main/java/com/dezdeqness/muzika/presentation/MainActivity.kt
+++ app/src/main/java/com/dezdeqness/muzika/presentation/MainActivity.kt
@@ -1,43 +1,42 @@
 package com.dezdeqness.muzika.presentation
 
+import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.material.ripple.RippleAlpha
-//import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.RippleConfiguration
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.dezdeqness.muzika.core.ui.rememberBottomSheetState
 import com.dezdeqness.muzika.presentation.features.home.Home
 import com.dezdeqness.muzika.presentation.features.navigation.HOME_ROUTE
-import com.dezdeqness.muzika.presentation.features.navigation.PLAYER_ROUTE
 import com.dezdeqness.muzika.presentation.features.navigation.SEARCH_ROUTE
-import com.dezdeqness.muzika.presentation.features.player.MiniPlayer
 import com.dezdeqness.muzika.presentation.features.player.Player
 import com.dezdeqness.muzika.presentation.features.search.Search
 import com.dezdeqness.muzika.service.MusicService
@@ -50,6 +49,8 @@ class MainActivity : AppCompatActivity() {
     private var controllerFuture: ListenableFuture<MediaController>? = null
     private var playbackConnection by mutableStateOf<PlaybackConnection?>(null)
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedBoxWithConstraintsScope")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -60,7 +61,6 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             MaterialTheme {
-
                 val rippleTheme = RippleConfiguration(
                     rippleAlpha = RippleAlpha(
                         pressedAlpha = 0.48f,
@@ -76,26 +76,34 @@ class MainActivity : AppCompatActivity() {
                     LocalPlaybackConnection provides playbackConnection,
                 ) {
                     val navController = rememberNavController()
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
 
-                    Box(
+                    BoxWithConstraints(
                         modifier = Modifier
                             .background(Color.Black)
                             .fillMaxSize()
                     ) {
+                        val playerBottomSheetState = rememberBottomSheetState(
+                            dismissedBound = 0.dp,
+                            collapsedBound = WindowInsets.navigationBars.asPaddingValues()
+                                .calculateBottomPadding() + 72.dp,
+                            expandedBound = maxHeight,
+                        )
 
-                        var isMiniPlayerVisible by remember {
-                            mutableStateOf(false)
-                        }
+                        val connection =
+                            LocalPlaybackConnection.current ?: return@BoxWithConstraints
 
+                        val currentMediaItem = connection.currentMediaItem.collectAsState()
 
-                        isMiniPlayerVisible = when (navBackStackEntry?.destination?.route) {
-                            PLAYER_ROUTE -> {
-                                false
-                            }
-
-                            else -> {
-                                true
+                        LaunchedEffect(currentMediaItem.value) {
+                            val mediaItem = playbackConnection?.currentMediaItem
+                            if (mediaItem == null) {
+                                if (!playerBottomSheetState.isDismissed) {
+                                    playerBottomSheetState.dismiss()
+                                }
+                            } else {
+                                if (playerBottomSheetState.isDismissed) {
+                                    playerBottomSheetState.collapseSoft()
+                                }
                             }
                         }
 
@@ -107,25 +115,12 @@ class MainActivity : AppCompatActivity() {
                             composable(HOME_ROUTE) {
                                 Home(navController = navController)
                             }
-
-                            composable(PLAYER_ROUTE) {
-                                Player()
-                            }
-
                             composable(SEARCH_ROUTE) {
                                 Search()
                             }
                         }
 
-                        MiniPlayer(
-                            isVisible = isMiniPlayerVisible,
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .align(Alignment.BottomCenter),
-                            onPlayerClicked = {
-                                navController.navigate(PLAYER_ROUTE)
-                            },
-                        )
+                        Player(state = playerBottomSheetState)
                     }
                 }
             }
