d32665fd563e90f10e1ffd424fef6164107e677c
Danyl Brahin <inicerer141@gmail.com>
2025-01-22 23:30:28 +0200

    Remove route

--- app/src/main/java/com/dezdeqness/muzika/presentation/features/navigation/Screens.kt
+++ app/src/main/java/com/dezdeqness/muzika/presentation/features/navigation/Screens.kt
@@ -3,5 +3,4 @@ package com.dezdeqness.muzika.presentation.features.navigation
 const val HOME_ROUTE = "/home"
 const val LIBRARY_ROUTE = "/library"
 const val SEARCH_ROUTE = "/search"
-const val PLAYER_ROUTE = "/player"
 const val SETTINGS_ROUTE = "/settings"
