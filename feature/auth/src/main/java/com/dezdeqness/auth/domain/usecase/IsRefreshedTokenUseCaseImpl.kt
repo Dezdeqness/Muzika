7bd1bd55524255550ad0166466df4b5c9b215f2b
Danyl Brahin <inicerer141@gmail.com>
2025-05-13 15:32:51 +0300

    Add IsRefreshedToken use case

---
+++ feature/auth/src/main/java/com/dezdeqness/auth/domain/usecase/IsRefreshedTokenUseCaseImpl.kt
@@ -0,0 +1,23 @@
+package com.dezdeqness.auth.domain.usecase
+
+import com.dezdeqness.auth.data.provider.TokenDataProvider
+import com.dezdeqness.auth.domain.repository.AuthRepository
+import com.dezdeqness.core.network.domain.IsRefreshedTokenUseCase
+
+class IsRefreshedTokenUseCaseImpl(
+    private val authRepository: AuthRepository,
+    private val tokenDataProvider: TokenDataProvider,
+) : IsRefreshedTokenUseCase {
+    override suspend operator fun invoke(): Result<Boolean> {
+
+        if (authRepository.isTokenExpired().not()) {
+            return Result.success(false)
+        }
+
+        return authRepository
+            .refreshToken()
+            .onSuccess { tokenDataProvider.setTokenData(it) }
+            .map { true }
+    }
+
+}
